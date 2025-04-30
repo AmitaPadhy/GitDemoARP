3ccf20c4e7269498a24bc6695cb86769f513d930
AmitaPadhy <60314042+AmitaPadhy@users.noreply.github.com>
2025-04-30 22:40:34 +0530

    Update Cherry.java

--- src/Day2/Cherry.java
+++ src/Day2/Cherry.java
@@ -5,6 +5,7 @@ public class Cherry extends Fruit {
 	public Cherry(String name) {
 		super(name);
 		System.out.println("In Cherry Constructor");
+		System.out.println("In Cherry Constructor copy code");
 	}
 
 
